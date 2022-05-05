599ff940976ad14ca9a528f4f53a7129a3874731
Julia claudia De melo viknander <jde-melo@c1r11s6.42lisboa.com>
2022-05-05 13:29:52 +0100

    👸

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,18 +6,21 @@
 /*   By: jde-melo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 12:40:53 by jde-melo          #+#    #+#             */
-/*   Updated: 2022/05/03 12:04:15 by jde-melo         ###   ########.fr       */
+/*   Updated: 2022/05/05 13:12:20 by jde-melo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef	struct s_list
+#include "../libft/libft.h"
+
+typedef	struct a_stack
 {
 	int				content;
-	int				pos;
-	struct s_list	*next;
-}					t_list;
+	int				position;
+	struct a_stack	*next;
+	struct a_stack	*prev;
+}	a_stack;
 
 #endif
--- srcs/lst_new.c
+++ srcs/lst_new.c
@@ -6,16 +6,22 @@
 /*   By: jde-melo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 11:19:41 by jde-melo          #+#    #+#             */
-/*   Updated: 2022/05/03 11:41:38 by jde-melo         ###   ########.fr       */
+/*   Updated: 2022/05/05 13:29:05 by jde-melo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_list	*lst_new(int *content)
+a_stack	*lst_new(int content)
 {
-	element = (t_list *)malloc(sizeof(t_list));
+	a_stack	*element;
+
+	element = (a_stack *)malloc(sizeof(a_stack));
 	if (!element)
-		retunr (NULL);
+		return (NULL);
 	element->content = content;
 	element->next = NULL;
+	element->pos = 0;
+	element->prev = NULL;
 	return (element);
 }
+
+struct *list = NULL;
